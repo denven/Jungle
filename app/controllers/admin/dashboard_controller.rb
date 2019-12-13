afb51ae6b44c0db7c3b796a66644507c6e80ed1a
denven <myregs@foxmail.com>
2019-12-12 22:08:03 -0800

    Added Two tables of Product and Categories statistics

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,21 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
+
+    @categories = []
+    getCategories
+
+  end
+
+  private
+  def getCategories
+    Category.all.each do |category| 
+      @categories.push(
+        :name => category.name, 
+        :count => Product.where(category_id: category.id).count,
+        :total => Product.where(category_id: category.id).sum(:quantity)
+        )
+    end
   end
 end
