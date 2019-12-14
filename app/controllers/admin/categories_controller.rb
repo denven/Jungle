7204c2fab058e6a5dc9454560f5f7cd8cb07ce3b
denven <myregs@foxmail.com>
2019-12-13 23:06:09 -0800

    Added categories controller and views

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,8 @@
 class Admin::CategoriesController < ApplicationController
 
-   def index
-    @categories = Category.order(id: :desc).all
+  def index
+    @categories = Category.order(id: :asc).all
+    # getCategories
   end
 
   def new
@@ -11,7 +12,7 @@ class Admin::CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
 
-    if @product.save
+    if @category.save
       redirect_to [:admin, :categories], notice: 'category created!'
     else
       render :new
@@ -19,6 +20,16 @@ class Admin::CategoriesController < ApplicationController
   end
 
   private
+
+  def getCategories
+    Category.all.each do |category| 
+      @categories.push(
+        :id => category.id,
+        :name => category.name, 
+        :products => Product.where(category_id: category.id)
+        )
+    end
+  end
 
   def category_params
     params.require(:category).permit(
