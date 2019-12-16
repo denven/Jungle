2b8d7c85231e391948501e7162bf33c41b1249db
denven <myregs@foxmail.com>
2019-12-15 22:27:39 -0800

    Added validates for using existed email to signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
@@ -8,7 +9,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to signup
+      redirect_to '/signup'
     end
   end 
 
