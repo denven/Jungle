a05d6340bbf5ea71362475849f51d1cc30308783
denven <myregs@foxmail.com>
2019-12-17 01:18:52 -0800

    Added rspec test for user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates_uniqueness_of :email, :message => '%{value} has already been taken'
+  validates_uniqueness_of :email, case_sensitive: false, :message => '%{value} has already been taken'
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates_confirmation_of :password_confirmation
+   
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  user = User.create(first_name: "Hans", last_name: "Ng", email: "test@test.com", password: "123", password_confirmation: "123")
+
+  describe 'Validations' do
+    # validation examples here
+    subject { described_class.new(first_name: "Hans", last_name: "Ng", email: "123@test.com", password: "123", password_confirmation: "123") }
+
+    it "validates: user" do
+      expect(subject).to be_valid
+    end
+
+    it "validates: password" do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "validates: password_confirmation" do
+      subject.password_confirmation = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "validates: password matches" do
+      subject.password ="123"
+      subject.password_confirmation = "1234"
+      expect(subject).to_not be_valid
+    end
+
+    it "validates: email in-casesensitive" do
+      subject.email = "tEst@TEST.COM"
+      expect(subject).to_not be_valid
+    end
+
+  end
+
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+  end
+
+
+end
