13feaa45e871cda781ac550ad0350da4816847b4
denven <myregs@foxmail.com>
2019-12-17 16:10:32 -0800

    Modified http_basic_authentication routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD'], except: :index
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD'], only: :orders
   protect_from_forgery with: :exception
 
   private
